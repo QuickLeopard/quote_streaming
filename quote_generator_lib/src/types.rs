6a55835d5a8d6108d00b4bf3c0cf8601d2a5636b
Igor Slepnev <slepneviv@gmail.com>
2025-12-24 11:34:08 +0500

    Add StockQuote constructor function 'new'

--- quote_generator_lib/src/types.rs
+++ quote_generator_lib/src/types.rs
@@ -5,3 +5,14 @@ pub struct StockQuote {
     pub volume: u32,
     pub timestamp: u64,
 }
+
+impl StockQuote {
+    pub fn new(ticker: &str, price: f64, volume: u32, timestamp: u64) -> Self {
+        Self {
+            ticker: ticker.to_string(),
+            price,
+            volume,
+            timestamp,
+        }
+    }
+}
